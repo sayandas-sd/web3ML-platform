69627d16b8728d154c0fe73f8ad501c279eaa4ab
sayandas-sd <sayandasmac@gmail.com>
2024-11-16 11:45:44 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+
+app.get("/", (req,res) => {
+    res.send("helo")
+})
+
+app.listen(port, ()=>{
+    console.log(`server is running at port ${port}`)
+})
+
