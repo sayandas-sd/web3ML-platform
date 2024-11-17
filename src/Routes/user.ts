7c6c2c3cbd67284b79a73ce39dfb50d434183ba2
sayandas-sd <sayandasmac@gmail.com>
2024-11-17 19:43:45 +0530

    added user authentication

--- src/Routes/user.ts
+++ src/Routes/user.ts
@@ -1,12 +1,36 @@
 import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = "123saya";
+
 const router = Router();
 
 
 const prisma = new PrismaClient();
 
 router.post("/", async (req,res) => {
-    
+    const publicKey = "HtkgKvwwJdEwq3EpwwCtVcHqvZed1Davc1wCB4JQkzcZ";
+
+    const ExistUser = await prisma.user.findFirst({
+        where: {
+            address: publicKey
+        }
+    })
+
+    if(ExistUser) {
+        const token = jwt.sign({
+            userId: publicKey
+        }, JWT_SECRET)
+    } else {
+        const user = await prisma.user.create({
+            data: {
+                address: publicKey
+            }
+        })
+    }
+
+
 });
 
 export default router;
